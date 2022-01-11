61a565106daaba373d1df6dc3718f837d775621a
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2022-01-11 16:22:58 +0530

    added technology component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
 
 @NgModule({
