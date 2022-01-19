6b8f7a400611677589f058f9a8c8b29230912850
kiranrajakula796 <kiranrajakula796@gmail.com>
2022-01-19 12:42:50 +0530

    table and form for client screen

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-client',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  onDeleteClick() {
+    console.log("delete")
+  }
+  onUpdateClick() {
+    console.log("update")
+  }
+  navigateToCreateForm(){
+    this.router.navigate(['/client/create'])
+  }
+  navigateToUpdateForm(){
+    this.router.navigate(['/client/update',1])
+  }
 }
