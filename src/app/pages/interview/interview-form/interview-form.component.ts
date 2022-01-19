8cc0702973fcf25eec1af860657ef3a586d275b7
kiranrajakula796 <kiranrajakula796@gmail.com>
2022-01-19 17:17:31 +0530

    input fields for interviewform component

--- src/app/pages/interview/interview-form/interview-form.component.ts
+++ src/app/pages/interview/interview-form/interview-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-interview-form',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterviewFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+  public interviewForm:FormGroup;
 
   ngOnInit(): void {
+    this.buildForm();
   }
-
+  buildForm() {
+    this.interviewForm = this.fb.group({
+      
+      employeeId: [''],
+      traineeId: [''],
+      clientId: [''],
+      interviewDate: [''],
+      endclient: [''],
+      allotedtime  : [''],
+      status: [''],
+      description: [''],
+    });
+  }
+createForm(){
+  console.log(this.interviewForm.value)
+}
 }
