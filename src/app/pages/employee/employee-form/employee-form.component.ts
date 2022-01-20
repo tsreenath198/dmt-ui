378e3611caff7e035dabfb1576c809265fea0ef2
kiranrajakula796 <kiranrajakula796@gmail.com>
2022-01-20 10:55:16 +0530

    input fields for employeeForm Component

--- src/app/pages/employee/employee-form/employee-form.component.ts
+++ src/app/pages/employee/employee-form/employee-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  public employeeForm: FormGroup;
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.employeeForm = this.fb.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      phone: [''],
+      email: [''],
+      role: [''],
+      salary: [''],
+      address: [''],
+      description: [''],
+    });
+  }
+
+  createForm() {
+    console.log(this.employeeForm.value);
   }
 
 }
