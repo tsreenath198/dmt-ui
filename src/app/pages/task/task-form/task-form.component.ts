03c19ee385b0fc7a019c2ae0affa44bb36bf7a12
kiranrajakula796 <kiranrajakula796@gmail.com>
2022-01-21 10:28:26 +0530

    Input Fields for taskForm Component

--- src/app/pages/task/task-form/task-form.component.ts
+++ src/app/pages/task/task-form/task-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+  public taskForm:FormGroup;
 
   ngOnInit(): void {
+    this.buildForm();
   }
-
+  buildForm() {
+    this.taskForm = this.fb.group({
+      
+      employeeId: [''],
+      category: [''],
+      status: [''],
+      estimatedTime  : [''],
+      date: [''],
+      description: [''],
+    });
+  }
+createForm(){
+  console.log(this.taskForm.value)
+}
 }
