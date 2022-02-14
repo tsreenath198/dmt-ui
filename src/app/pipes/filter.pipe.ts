491ef212571429e9faba066f2dc8072ce176c4c9
kiranrajakula796 <kiranrajakula796@gmail.com>
2022-02-14 18:03:12 +0530

    updated unput search field for all components

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(technologyDataSources: any[], field:string, value:string): any {
+  transform(technologyDataSources: any[], field:string, value:string): any[] {
 
   if(!technologyDataSources ){
     return [];
