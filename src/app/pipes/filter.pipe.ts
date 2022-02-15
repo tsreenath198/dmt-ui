97aa5c8c509dbe02ab38c50eabc62ba53d2405de
gangasaiuskcorp <gangasai.uskcorp@gmail.com>
2022-02-15 18:00:46 +0530

    refactore

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -14,14 +14,14 @@ export class FilterPipe implements PipeTransform {
     if (!searchString || !dataSources) {
       return dataSources;
     }
-    return dataSources.filter((singletechnologyDataSource) =>
-      singletechnologyDataSource.name?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
-      singletechnologyDataSource.id?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
-      singletechnologyDataSource.phone?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
-      singletechnologyDataSource.email?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
-      singletechnologyDataSource.endClient?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
-      singletechnologyDataSource.invoiceNo?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
-      singletechnologyDataSource.no?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase())
+    return dataSources.filter((dataSource) =>
+      dataSource.name?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      dataSource.id?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      dataSource.phone?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      dataSource.email?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      dataSource.endClient?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      dataSource.invoiceNo?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      dataSource.no?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase())
 
 
     );  
