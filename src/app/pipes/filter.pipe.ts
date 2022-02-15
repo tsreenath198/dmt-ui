fdd8dde20ca522963d9d4f1fca93f22e2b2ecf03
kiranrajakula796 <kiranrajakula796@gmail.com>
2022-02-15 12:38:12 +0530

    Updated  table search filter for all components

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,21 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TechnologyModel } from 'app/models/technology-model';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(technologyDataSources: any[], field:string, value:string): any[] {
+  transform(technologyDataSources: any[], searchString: string): any[] {
 
-  if(!technologyDataSources ){
-    return [];
-  }
-  if(!field || ! value) {
-    return technologyDataSources;
-  }
+    if (!technologyDataSources) {
+      return [];
+    }
+    if (!searchString || !technologyDataSources) {
+      return technologyDataSources;
+    }
     return technologyDataSources.filter((singletechnologyDataSource) =>
-        singletechnologyDataSource[field].toLowerCase().includes(value.toLowerCase())
-    );
+      singletechnologyDataSource.name?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      singletechnologyDataSource.id?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      singletechnologyDataSource.phone?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      singletechnologyDataSource.email?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      singletechnologyDataSource.endClient?.toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      singletechnologyDataSource.invoiceNo?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase()) ||
+      singletechnologyDataSource.no?.toString().toLocaleLowerCase().match(searchString.toLocaleLowerCase())
+
+
+    );  
   }
 
 }
